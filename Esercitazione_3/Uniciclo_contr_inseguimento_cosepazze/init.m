767cf9b43b9f882e6bf7ae5a8cf1dfef1846d121
Vincenzo Degiacomo <vincenzo.degiacomo98@gmail.com>
2021-11-10 10:37:07 +0100

    RAS
    Siamo giunti alla conclusione che la lyapunov scelta non è radialmente illimitata e che la Ras è abbastanza piccola

--- Esercitazione_3/Uniciclo_contr_inseguimento_cosepazze/init.m
+++ Esercitazione_3/Uniciclo_contr_inseguimento_cosepazze/init.m
@@ -1,5 +1,5 @@
 % controllo di posizione unicilco
-close all;
+%close all;
 clear;
 clc;
 
@@ -7,13 +7,13 @@ clc;
 %q0 = [-10; 20; pi/4];
 %q0 = [20; -10; pi/2];
 %q0 = [-10; -10; -pi];
-q0 = [-40; 5; pi/2];
+q0 = [40; 5; pi/2];
 
-% posizione iniziale fugitivo
+% posizione iniziale fugitivo (RAS stimata tra 28.375 e 28.38)
 %q0 = [-10; 20; pi/4];
 %q0 = [20; -10; pi/2];
 %q0 = [-10; -10; -pi];
-q0_hat = [6; -15; pi];
+q0_hat = [8; 2; -pi];
 
 
 % guadagni controllo
@@ -24,5 +24,5 @@ kl = [l1 l2 k];
 % v_hat = 10;
 
 v0_hat = 10;
-kth = 1;
-r0 = 10;
+kv = 10;
+r0 = 1;
