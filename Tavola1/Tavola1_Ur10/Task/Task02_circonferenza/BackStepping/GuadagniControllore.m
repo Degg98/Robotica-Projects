41be897a32dfbe6cae021b16727993ee2e554655
Pietro Gori <p.gori3@studenti.unipi.it>
2022-10-06 14:25:12 +0100

    Aggiustati controlli circonferenza con saturazione sulle coppie

--- Tavola1/Tavola1_Ur10/Task/Task02_circonferenza/BackStepping/GuadagniControllore.m
+++ Tavola1/Tavola1_Ur10/Task/Task02_circonferenza/BackStepping/GuadagniControllore.m
@@ -1,3 +1,5 @@
-Kd = diag([5 5 9 9 15 15]);
-lambda = diag([2 2 5 12 4 10]);
+% Kd = 20*diag([5 5 9 9 15 15]);
+% lambda = 1*diag([2 2 5 7 1 1]);
 %convergenza a zero dell'errore
+Kd = diag([100 500 300 10 2 0.1]);
+lambda = diag([20 30 30 12 50 500]);
--- Tavola1/Tavola1_Ur10/Task/Task02_circonferenza/PD/GuadagniControllore.m
+++ Tavola1/Tavola1_Ur10/Task/Task02_circonferenza/PD/GuadagniControllore.m
@@ -1,5 +1,12 @@
 %CT task 1
-Kp = diag([6 6 9 9 12 12]);
-Kv = diag([12 11 15 15 20 20]);
+Kp = 10*diag([30 30 27 20 22 22]);
+Kv = 10*diag([35 35 25 20 22 22]);
 
 %convergenza a zero dell'errore
+
+m1 = L(1).m;
+m2 = L(2).m;
+m3 = L(3).m;
+m4 = L(4).m;
+m5 = L(5).m;
+m6 = L(6).m;
