41303e3bb441396f30cc260b75dcb13e8553d114
Pietro Gori <p.gori3@studenti.unipi.it>
2022-10-06 11:15:20 +0100

    Aggiunta saturazione delle coppie

--- Tavola1/Tavola1_Ur10/Task/Task01_pick&place/Li_Slotine/GuadagniControllore.m
+++ Tavola1/Tavola1_Ur10/Task/Task01_pick&place/Li_Slotine/GuadagniControllore.m
@@ -1,7 +1,7 @@
 %% LI-SLOTINE
 %Pick and Place
-Kd =100 * diag([1 5 3 1 1 1]);
-lambda = 1*diag([20 30 30 12 10 50]);
+Kd = 100 * diag([1 10 7 1 1 1]);
+lambda = diag([20 30 30 12 10 50]);
 
 m1 = L(1).m;
 m2 = L(2).m;
