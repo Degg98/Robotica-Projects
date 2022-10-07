ab1a2d3eb9c1dec09e8b77b25562e142ac873340
Pietro Gori <p.gori3@studenti.unipi.it>
2022-10-07 10:24:11 +0100

    Adaptive BS su circonferenza ok

--- Tavola1/Tavola1_Ur10/Task/Task02_circonferenza/Adaptive_Backstepping/GuadagniControllore.m
+++ Tavola1/Tavola1_Ur10/Task/Task02_circonferenza/Adaptive_Backstepping/GuadagniControllore.m
@@ -1,4 +1,12 @@
 %% Adaptive BS
-%Pick and Place
-Kd =100 * diag([1 5 3 1 1 1]);
-lambda = 1*diag([20 30 30 12 10 50]);
+% Circle
+Kd = diag([100 500 300 10 2 0.1]);
+lambda = diag([30 70 40 300 50 500]);
+
+% Link mass
+m1 = L(1).m;
+m2 = L(2).m;
+m3 = L(3).m;
+m4 = L(4).m;
+m5 = L(5).m;
+m6 = L(6).m;
